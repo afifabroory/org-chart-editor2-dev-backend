192b94f666fe8b840bda8bc7eeb8086955a124cb
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-12-30 09:26:22 +0800

    Fix CORS

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
-from routes.api import endpoint
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+from routes.api import endpoint
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 app.include_router(endpoint)
 
+
+
 if __name__ == '__main__':
-    uvicorn.run(app, port=8080)
+    uvicorn.run('main:app', port=8080, reload=True)
--- routes/api.py
+++ routes/api.py
@@ -6,50 +6,120 @@ endpoint = APIRouter(prefix="/api")
 @endpoint.get('/image/assets', response_model=dict[str, list[ImageAsset]])
 async def get_image_assets(response: Response):
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Content-Type"] = "application/json"
+    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
 
     return {
-        "High-Res": [
+        "Low Quality": [
             {
-                "name": "NAME_1",
-                "position": "POSITION_1",
-                "photo": "/path/to/files"
+                "name": "Ventje Rahardjo",
+                "position": "Direktur Eksekutif",
+                "photo": "/storage/upload/thumb/1672130209-ventje-rahadjo-soedigno.png"
             },
             {
-                "name": "NAME_2",
-                "position": "POSITION_2",
-                "photo": "/path/to/files"
+                "name": "Afdhal Aliasar",
+                "position": "Direktur Industri Produk Halal",
+                "photo": "/storage/upload/thumb/1672130201-afdhal-aliasar.png"
             },
             {
-                "name": "NAME_3",
-                "position": "POSITION_3",
-                "photo": "/path/to/files"
+                "name": "Taufik Hidayat",
+                "position": "Direktur Jasa Keuangan Syariah",
+                "photo": "/storage/upload/thumb/1672130188-taufiq-hidayat.png"
             },
             {
-                "name": "NAME_4",
-                "position": "POSITION_4",
-                "photo": "/path/to/files"
+                "name": "Ahmad Juwaini",
+                "position": "Direktur Keuangan Sosial Syariah",
+                "photo": "/storage/upload/thumb/1672130178-ahmad-juwaini.png"
+            },
+            {
+                "name": "Putu Rahwidhiyasa",
+                "position": "Direktur Bisnis dan Kewirausahaan Syariah",
+                "photo": "/storage/upload/thumb/1597624639-Putu Rahwidhiyasacircle.png"
+            },
+            {
+                "name": "Sutan Emir Hidayat",
+                "position": "Direktur Infrastruktur Ekosistem Syariah",
+                "photo": "/storage/upload/thumb/1672130164-sutan-emir-hidayat.png"
+            },
+            {
+                "name": "Presiden RI",
+                "position": "Ketua",
+                "photo": "/storage/upload/thumb/1672129110-presiden.png"
+            },
+            {
+                "name": "Wakil Presiden RI",
+                "position": "Ketua Harian",
+                "photo": "/storage/upload/thumb/1672129933-wakil-presiden.png"
+            },
+            {
+                "name": "Kementerian Keuangan",
+                "position": "Sekertaris",
+                "photo": "/storage/upload/thumb/1672130266-img-kemenkeu.png"
+            },
+            {
+                "name": "Kominte Nasional Ekonomi dan Keuangan Syariah",
+                "position": "Manajemen Eksekutif",
+                "photo": "/storage/upload/thumb/1672130481-img-knks_old_0721a.png"
+            },
+            {
+                "name": "Anggota",
+                "position": "Anggota",
+                "photo": "/storage/upload/thumb/1672135174-anggota.png"
             }
-        ], "Low-Res": [
+        ], "High Quality": [
             {
-                "name": "NAME_1",
-                "position": "POSITION_1",
-                "photo": "/path/to/files"
+                "name": "Ventje Rahardjo",
+                "position": "Direktur Eksekutif",
+                "photo": "/storage/upload/1672130209-ventje-rahadjo-soedigno.png"
             },
             {
-                "name": "NAME_2",
-                "position": "POSITION_2",
-                "photo": "/path/to/files"
+                "name": "Afdhal Aliasar",
+                "position": "Direktur Industri Produk Halal",
+                "photo": "/storage/upload/1672130201-afdhal-aliasar.png"
             },
             {
-                "name": "NAME_3",
-                "position": "POSITION_3",
-                "photo": "/path/to/files"
+                "name": "Taufik Hidayat",
+                "position": "Direktur Jasa Keuangan Syariah",
+                "photo": "/storage/upload/1672130188-taufiq-hidayat.png"
             },
             {
-                "name": "NAME_4",
-                "position": "POSITION_4",
-                "photo": "/path/to/files"
+                "name": "Ahmad Juwaini",
+                "position": "Direktur Keuangan Sosial Syariah",
+                "photo": "/storage/upload/1672130178-ahmad-juwaini.png"
+            },
+            {
+                "name": "Putu Rahwidhiyasa",
+                "position": "Direktur Bisnis dan Kewirausahaan Syariah",
+                "photo": "/storage/upload/1597624639-Putu Rahwidhiyasacircle.png"
+            },
+            {
+                "name": "Sutan Emir Hidayat",
+                "position": "Direktur Infrastruktur Ekosistem Syariah",
+                "photo": "/storage/upload/1672130164-sutan-emir-hidayat.png"
+            },
+            {
+                "name": "Presiden RI",
+                "position": "Ketua",
+                "photo": "/storage/upload/1672129110-presiden.png"
+            },
+            {
+                "name": "Wakil Presiden RI",
+                "position": "Ketua Harian",
+                "photo": "/storage/upload/1672129933-wakil-presiden.png"
+            },
+            {
+                "name": "Kementerian Keuangan",
+                "position": "Sekertaris",
+                "photo": "/storage/upload/1672130266-img-kemenkeu.png"
+            },
+            {
+                "name": "Kominte Nasional Ekonomi dan Keuangan Syariah",
+                "position": "Manajemen Eksekutif",
+                "photo": "/storage/upload/1672130481-img-knks_old_0721a.png"
+            },
+            {
+                "name": "Anggota",
+                "position": "Anggota",
+                "photo": "/storage/upload/1672135174-anggota.png"
             }
         ]
     }
